c8df02ab923ef8d87ae135247bf3adc61783f689
coco <1316678514@qq.com>
2023-08-25 18:51:59 +0800

    注册页面

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,18 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [
-
+  {
+    path: '/reg',
+    component: () => {
+      import('@/views/register')
+    }
+  },
+  {
+    path: '/login',
+    component: () => {
+      import('@/views/login')
+    }
+  }
 ]
 
 const router = new VueRouter({
