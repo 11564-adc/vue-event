473d1f3d6e15b328b75065096493e2b1fe19791c
coco <1316678514@qq.com>
2023-08-25 16:05:22 +0800

    项目初始化

---
+++ src/api/index.js
@@ -0,0 +1,11 @@
+import request from '@/utils/request'
+export const registerAPI = () => {
+  return request({
+
+  })
+}
+export const getListAPI = () => {
+  return request({
+    url: ''
+  })
+}
